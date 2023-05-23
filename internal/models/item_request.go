2d9bc6cbbcabbb375f805469b52ffe336b3604b7
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-05-23 18:20:19 -0300

    [feat] add validation rule

--- internal/models/item_request.go
+++ internal/models/item_request.go
@@ -1,7 +1,31 @@
 package models
 
+import "github.com/go-playground/validator/v10"
+
 type ItemRequest struct {
 	Name     string `json:"name"`
 	Price    int    `json:"price"`
 	Quantity int    `json:"quantity"`
+}
+
+func (itemInput ItemRequest) ValidateStruct() []*ErrorResponse {
+	// create a variable to store validation errors
+	var errors []*ErrorResponse
+	// create a new validator
+	validate := validator.New()
+	// validate the struct
+	err := validate.Struct(itemInput)
+	// if the validation is failed
+	// insert the error inside "errors" variable
+	if err != nil {
+		for _, err := range err.(validator.ValidationErrors) {
+			var element ErrorResponse
+			element.ErrorMessage = getErrorMessage(err)
+			element.Field = err.Field()
+			errors = append(errors, &element)
+		}
+	}
+
+	// return the validation errors
+	return errors
 }
